1510f40083b197f9db1daa0fc4e77172dbd3a2aa
Luca Lambiase <luca.lambia@gmail.com>
2023-06-27 06:10:47 +0200

    page layouts and templates

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $links = config('db.someLinks');
+    return view('welcome', compact('links'));
+});
+
+Route::get('/otherpage', function () {
+    $links = config('db.someLinks');
+    return view('other', compact('links'));
 });
